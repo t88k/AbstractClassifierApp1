ac5181eae460319ea1d0d6c114bb96222979700a
t88k <47161086+t88k@users.noreply.github.com>
2021-05-25 23:29:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 from flask import Flask,render_template,url_for,request
 import joblib
+from textpreprocessing import pre_process
 
 pipeline = joblib.load('abstract_classification.joblib')
 
 topics = {1:'Computer Science', 2:'Physics', 3:'Mathematics', 4:'Statistics', 5:'Quantitative Biology', 6:'Quantitative Finance'}
 
 app = Flask(__name__)
-
-def pre_process(text):
-  #Make lowercase
-  text = text.strip().lower()
-
-  #Remove punctuation
-  filters = '!"\'#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'
-  translate_dict = dict((c, " ") for c in filters)
-  translate_map = str.maketrans(translate_dict)
-  text = text.translate(translate_map)
-
-  return text
 
 @app.route('/')
 def home():
@@ -34,6 +23,4 @@ def predict():
 
 
 if __name__ == '__main__':
-	pre_process = pre_process()
-	
 	app.run(debug=True)
